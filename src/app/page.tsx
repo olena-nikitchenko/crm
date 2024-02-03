61dd1827c0155748e159e752296e1c935611714c
olena-nikitchenko <helnikitka@gmail.com>
2024-02-03 20:24:31 +0200

    added new function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import StatusLabel, { Status } from "./components/status-lable";
 export default function Home() {
 	return (
 		<main>
-			<h1 className="text-xl">Home page</h1>
+			<h1 className="text-xl">Home page{new Date().toTimeString()}</h1>
 			<StatusLabel status={Status.Active}> Active</StatusLabel>
 			<StatusLabel status={Status.NotActive}> Not Active</StatusLabel>
 			<StatusLabel status={Status.Pending}> Pending</StatusLabel>
