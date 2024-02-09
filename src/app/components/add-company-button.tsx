f36f25f8a124eaf7f118b5990948dbd27bd84c1e
olena-nikitchenko <helnikitka@gmail.com>
2024-02-09 20:18:25 +0200

    added new components

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,12 +1,40 @@
+// "use client";
+
+// import React from "react";
+// import { useRouter } from "next/navigation";
+// import Button from "@/app/components/button";
+
+// export default function AddCompanyButton() {
+// 	const router = useRouter();
+// 	return (
+// 		<Button onClick={() => router.push("/companies/new", { scroll: false })}>Add company</Button>
+// 	);
+// }
+
 "use client";
 
-import React from "react";
-import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import Button from "@/app/components/button";
+import CompanyFormModal from "@/app/components/company-form-modal";
 
 export default function AddCompanyButton() {
-	const router = useRouter();
+	const [show, setShow] = useState(false);
 	return (
-		<Button onClick={() => router.push("/companies/new", { scroll: false })}>Add company</Button>
+		<>
+			<Button
+				onClick={() => {
+					setShow(true);
+				}}
+			>
+				Add company
+			</Button>
+			<CompanyFormModal
+				onSubmit={console.log}
+				show={show}
+				onClose={() => {
+					setShow(false);
+				}}
+			/>
+		</>
 	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import "../app/globals.css";
+// import React from 'react';
+import { Plus_Jakarta_Sans } from "next/font/google";
+import Providers from "@/app/components/providers";
+import "./globals.css";
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+const font = Plus_Jakarta_Sans({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className={font.className}>
+				<Providers>{children}</Providers>
+			</body>
 		</html>
 	);
 }
