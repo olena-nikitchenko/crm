c07901008da99d389ea329e84dac7bd1b2d692a8
olena-nikitchenko <helnikitka@gmail.com>
2024-02-10 19:39:29 +0200

    added otimization modal

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,21 +1,11 @@
-// "use client";
-
-// import React from "react";
-// import { useRouter } from "next/navigation";
-// import Button from "@/app/components/button";
-
-// export default function AddCompanyButton() {
-// 	const router = useRouter();
-// 	return (
-// 		<Button onClick={() => router.push("/companies/new", { scroll: false })}>Add company</Button>
-// 	);
-// }
-
 "use client";
 
 import React, { useState } from "react";
 import Button from "@/app/components/button";
-import CompanyFormModal from "@/app/components/company-form-modal";
+import dynamic from "next/dynamic";
+// import CompanyFormModal from "@/app/components/company-form-modal";
+
+const CompanyFormModal = dynamic(() => import("./company-form-modal"), { ssr: false });
 
 export default function AddCompanyButton() {
 	const [show, setShow] = useState(false);
