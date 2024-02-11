ac2d1124e2fd611e6565cfd237224fe8e2d4ad40
olena-nikitchenko <helnikitka@gmail.com>
2024-02-11 16:36:37 +0200

    added new server and client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import AddCompanyButton from "./components/add-company-button";
+import ClientComponent from "./components/client-component";
+import ServerComponent from "./components/server-component";
+import ServerComponentCopy from "./components/server-component-copy";
 
 export default function Home() {
 	return (
 		<main>
 			<h1 className="text-xl">Home page</h1>
 			<AddCompanyButton />
+			<ServerComponent />
+			<ClientComponent>
+				<ServerComponentCopy />
+			</ClientComponent>
 		</main>
 	);
 }
